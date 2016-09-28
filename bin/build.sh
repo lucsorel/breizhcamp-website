a74c312584eaa31b10961413962ec4b5f3a9fb00
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2016-09-28 18:26:07 +0200

    fix build

--- bin/build.sh
+++ bin/build.sh
@@ -5,14 +5,14 @@ set -eu -o pipefail
 PROJECT_ROOT="$(cd -P "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
 cd "$PROJECT_ROOT"
 
-HUGO=$(which hugo)
-if [[ $? -ne 0 ]]; then
+HUGO=$(which hugo) || not_found=$?
+if [[ $not_found -ne 0 ]]; then
   if [[ ! -d .hugo ]]; then
     mkdir .hugo
     curl -L https://github.com/spf13/hugo/releases/download/v0.15/hugo_0.15_linux_amd64.tar.gz | \
       (cd .hugo; tar xzf -)
   fi
-  HUGO=".hugo/hugo_0.15_linux_amd64/hugo_0.15_linux_amd64/hugo"
+  HUGO=".hugo/hugo_0.15_linux_amd64/hugo_0.15_linux_amd64"
 fi
 
 REPO="git@github.com:breizhcamp/www-staging.git"
@@ -31,7 +31,7 @@ done
 if [[ ! -d public ]]; then
   git clone -b gh-pages "$REPO" public
 else
-  ORIGIN_URL=$(git remote get-url origin)
+  ORIGIN_URL=$(git config --get remote.origin.url)
   if [[ "$REPO" != "$ORIGIN_URL" ]]; then
     rm -rf public
     git clone -b gh-pages "$REPO" public
