4de4a9f262d1a18dbae3b5c3fd61ac719bcd141d
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2017-03-08 17:06:48 +0100

    update speakers

--- bin/getSpeakers.groovy
+++ bin/getSpeakers.groovy
@@ -5,14 +5,14 @@ def urlString = "https://api.cfp.io/api/speakers/accepted"
 def url = new URL(urlString)
 def connection = url.openConnection()
 connection.setRequestMethod("GET")
-connection.setRequestProperty("Referer", "https://breizhcamp.cfp.io/")
-connection.setRequestProperty("accept", "application/json"); 
+connection.setRequestProperty("X-Tenant-Id", "breizhcamp")
+connection.setRequestProperty("accept", "application/json")
 
 connection.connect()
 
 if (connection.responseCode != 200) {
     println "HTTP Error for URL ${urlString}"
-    println "Error ${connection.responseCode} for talk ${talkId}"
+    println "Error ${connection.responseCode}"
 }
 else {
     println connection.content.text
