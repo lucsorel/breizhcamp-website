3726b4bffdc1ae254c8ec1aa808bb620616b71ee
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2023-07-21 16:31:21 +0200

    generate talks.json

--- bin/sort.sh
+++ bin/sort.sh
@@ -4,17 +4,14 @@ set -eu -o pipefail
 root_dir="$(cd -P "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 schedule_file="$root_dir/static/json/schedule.json"
+talks_file="$root_dir/static/json/talks.json"
+
 if [[ -r "$schedule_file" ]]; then
   content="$(cat "$schedule_file")"
   echo "$content" |\
     jq 'sort_by(.id)' |\
     jq -f "$root_dir/bin/format-talks-json.jq" > "$schedule_file"
-fi
-
-talks_file="$root_dir/static/json/talks.json"
-if [[ -r "$talks_file" ]]; then
-  content="$(cat "$talks_file")"
   echo "$content" |\
-    jq 'sort_by(.id)' |\
+    jq 'sort_by(.id) | map(. | select (.event_type != null))' |\
     jq -f "$root_dir/bin/format-talks-json.jq" > "$talks_file"
 fi
