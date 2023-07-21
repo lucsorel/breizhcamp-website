d4f23d15dc2fc141e724d5231fcdaf96732d2536
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2023-07-21 16:00:12 +0200

    Sort object structure too

--- bin/sort-schedule.sh
+++ bin/sort-schedule.sh
@@ -6,5 +6,7 @@ root_dir="$(cd -P "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 schedule_file="$root_dir/static/json/schedule.json"
 if [[ -r "$schedule_file" ]]; then
   schedule="$(cat "$schedule_file")"
-  echo "$schedule" | jq 'sort_by(.id)' > "$schedule_file"
+  echo "$schedule" |\
+    jq 'sort_by(.id)' |\
+    jq -f "$root_dir/bin/format-talks-json.jq" > "$schedule_file"
 fi
